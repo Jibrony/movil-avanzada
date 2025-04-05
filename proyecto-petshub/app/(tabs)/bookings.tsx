c7c00d900e8073738dc18e2be887c6ae07e200d8
Andrey <andrey23arce@gmail.com>
2025-04-05 09:31:23 -0700

    CREACION PANTALLA BOOKINGS
    Pantalla en la cual se podrá observar las reservas

---
+++ proyecto-petshub/app/(tabs)/bookings.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function BookingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Mis reservas</Text>
+    </View>
+  );
+}
